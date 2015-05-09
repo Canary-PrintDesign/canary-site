eb871aeb9ee6bb33d8a3ac52c086216d01234ca0
adamish717 <adam@piechnik.ca>
2015-05-09 11:29:02 -0700

    Clean up JS files

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,9 +1,16 @@
-//= require jquery.cubeportfolio.min
-//= require custom
-//= require jquery.parallax
-//= require mixitup
-//= require wow.min
-//= require portfolio
-//= require app
-//= require cube-portfolio-3
+//= require _jquery.2.1.4.min
+//= require _bootstrap.min
 
+// Design page effects and Lightbox
+//= require _jquery.cubeportfolio
+//= require _cube-portfolio-3
+
+// Print page and Popular Products area on Product Pages
+//= require _mixitup
+//= require _portfolio
+
+// Animates items when they enter the viewport
+//= require _wow.min
+
+// Nav change on-scroll
+//= require _app
