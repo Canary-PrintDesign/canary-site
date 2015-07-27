f161b581ee0df35bdd2f6c611dfc2dedd78c0ef1
Sinetheta <k.attfield@gmail.com>
2015-07-26 17:42:23 -0700

    Set caching policy

--- config.rb
+++ config.rb
@@ -104,3 +104,6 @@ activate :s3_sync do |s3_sync|
   s3_sync.prefix                     = ''
   s3_sync.version_bucket             = false
 end
+
+default_caching_policy max_age:(60 * 60 * 24 * 365)
+caching_policy 'text/html', max_age: 0, must_revalidate: true
