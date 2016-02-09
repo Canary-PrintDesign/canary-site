08ac062feb3fc1b91889ac15ab2429f3569cf6d1
Sinetheta <k.attfield@gmail.com>
2016-02-08 18:31:01 -0800

    really? still screwing with build?

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,15 @@
-if [ $TRAVIS_BRANCH = production ] ; then
+echo $TRAVIS_BRANCH
+echo $TRAVIS_PULL_REQUEST
+branch_name="$(git symbolic-ref --short -q HEAD)"
+echo $branch_name
+if [[ $TRAVIS_BRANCH = production && !$TRAVIS_PULL_REQUEST ]] ; then
+  echo 'dep www'
   export AWS_BUCKET='www.canaryprint.ca'
-elif [ $TRAVIS_BRANCH = master ] ; then
+elif [[ $TRAVIS_BRANCH = master && !$TRAVIS_PULL_REQUEST ]] ; then
+  echo 'dep staging'
   export AWS_BUCKET='staging.canaryprint.ca'
 else
+  echo 'dep dev'
   export AWS_BUCKET='dev.canaryprint.ca'
 fi
 
