f84854fa90220098b8320740df4b1716e20a226a
Sinetheta <k.attfield@gmail.com>
2016-02-08 18:31:01 -0800

    really? still screwing with build?

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,13 @@
-if [ $TRAVIS_BRANCH = production ] ; then
+echo "Travis branch: $TRAVIS_BRANCH"
+echo "Building pull request? $TRAVIS_PULL_REQUEST"
+
+if [[ $TRAVIS_BRANCH == production && !$TRAVIS_PULL_REQUEST ]] ; then
   export AWS_BUCKET='www.canaryprint.ca'
-elif [ $TRAVIS_BRANCH = master ] ; then
+elif [[ $TRAVIS_BRANCH == master && !$TRAVIS_PULL_REQUEST ]] ; then
   export AWS_BUCKET='staging.canaryprint.ca'
 else
   export AWS_BUCKET='dev.canaryprint.ca'
 fi
 
+echo "Deploying to $AWS_BUCKET"
 bundle exec middleman sync
